bf04150ebba707a0df83c77f93b7ce3f8f548362
JulieGunawan <48563515+JulieGunawan@users.noreply.github.com>
2024-02-25 15:37:27 -0700

    nullable and literal type

--- tutorial/src/index.ts
+++ tutorial/src/index.ts
@@ -1,14 +1,30 @@
-type Draggable = {
-    drag: () => void;
-}
+// type Draggable = {
+//     drag: () => void;
+// }
 
-type Resizeable = {
-    resize: () => void;
-}
+// type Resizeable = {
+//     resize: () => void;
+// }
 
-type UIWidget = Draggable & Resizeable;
+// type UIWidget = Draggable & Resizeable;
 
-let textBox: UIWidget = {
-    drag: () => {},
-    resize: () => {},
-}
+// let textBox: UIWidget = {
+//     drag: () => {},
+//     resize: () => {},
+// }
+
+//literal type (exact, specific)
+// type Quantity = 50 | 100;
+// let quantity: Quantity = 100;
+
+// type Metric = 'cm' | 'inch';
+
+//Nullable type
+// function greet(name:string | null | undefined){
+//     if (name)
+//         console.log(name.toUpperCase());
+//     else
+//         console.log("Hola!");
+// }
+
+// greet(null);
