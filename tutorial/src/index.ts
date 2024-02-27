0a13f26ddedfc73cfd16d626ed0c6cbbe78bef23
JulieGunawan <48563515+JulieGunawan@users.noreply.github.com>
2024-02-27 12:24:24 -0700

    class implements interface or type

--- tutorial/src/index.ts
+++ tutorial/src/index.ts
@@ -76,3 +76,63 @@ const user2:IUser = {
         return `Hello ${this.name}`
     },
 }
+
+//this way of defining type is valid, but not for interface
+type ID = string;
+
+//interface is related to entity or object, if we write it like type, it won't work
+// interface UserInterface{
+//     id: ID;
+//     name: string;
+//     surname: string;
+//     email: string;
+//     getPermission():string;
+// }
+
+// interface Admin extends UserInterface {
+//     permissions: string[];
+//     below code won't work, as we will get an error types returned are incompatible
+//     getPermission():string[] 
+// }
+
+type User = {
+    id: ID;
+    name: string;
+    surname: string;
+    email: string,
+    getPermission: (id: string) => string;
+}
+type Admin = User & {
+    permissions: string[];
+    //using type, it can overwrite the User getPermission function, but if we define in interface, it won't work
+    getPermission: (id: string[]) => string[];
+}
+
+const user1: Admin = {
+    id: "1",
+    name: "Alex",
+    surname: "S",
+    email: "a@a.com",
+    permissions: ["read", "write"],
+    getPermission(id: string[] | string) {
+        return (typeof id === "string" ? id : [id]) as string[] & string;
+    }
+}
+let username: string = "Alexandra";
+
+let pageName: string | number = "1";
+
+//you can create class to implement interface in typescript
+//for example like this
+
+//interface User {
+//     name: string;
+//     surname: string;
+//     email: string;
+// }
+
+// class User implements User {
+//     name: "Gru";
+//     surname: "G";
+//      email: "g@g.com";
+// }
