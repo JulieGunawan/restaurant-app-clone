6dd2cd758af211b6d1a733ecdc555c862df966b5
JulieGunawan <julie.gunawan@protonmail.com>
2025-02-04 17:38:05 -0700

    delete unused import

--- my-app/src/pages/menu/index.tsx
+++ my-app/src/pages/menu/index.tsx
@@ -1,6 +1,5 @@
 import MainLayout from "@/layouts/MainLayout";
 import { menus } from "@/utils/constants";
-import Image from "next/image";
 import Link from "next/link";
 import React  from "react";
 
