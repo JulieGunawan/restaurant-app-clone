83c5192f00a73ecddb7a9f0f595dd525cd0d0129
JulieGunawan <julie.gunawan@protonmail.com>
2025-03-10 11:00:32 -0600

    chore: Adding new models in prisma schema

--- my-app/src/utils/auth.ts
+++ my-app/src/utils/auth.ts
@@ -1,7 +1,10 @@
-import { NextAuthOptions } from "next-auth";
-
+import { getServerSession, NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "./connect";
+
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             //these as string vs ! is the same thing to force type to be string instead of undefined
@@ -10,3 +13,5 @@ export const authOptions: NextAuthOptions = {
         })
     ]
 }
+
+export const getAuthSession = () => getServerSession(authOptions)
